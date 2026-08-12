167fceb8b72459d9d60351da7769b973cbd12e38
sprincedeep10-ai <sprincedeep10@gmail.com>
2026-08-12 17:54:39 +0800

    chore: update about image version via staff panel

--- src/data/section-image-versions.ts
+++ src/data/section-image-versions.ts
@@ -2,5 +2,5 @@ export const SECTION_IMAGE_VERSIONS: Record<string, number> = {
   hero: 1786420220637,
   mission: 1786420166819,
   parents: 1786420256791,
-  about: 1786528361624,
+  about: 1786528478777,
 };
